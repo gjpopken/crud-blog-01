3a530912a7a29e09307e1e7e1837be73127a7d1d
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-26 15:38:46 -0600

    got two GET routes working

--- server/routes/blog.router.js
+++ server/routes/blog.router.js
@@ -4,15 +4,37 @@ const pool = require('../modules/pool.js');
 const router = express.Router();
 
 
-// GET /things - - - - - - - Respond with all the things.
+// GET /blog - - - - - - - Respond with all the blogs.
+router.get('/', (req, res) => {
+    const queryText = `
+    SELECT * FROM "blogs";`
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
-// GET /things/:id - - - - - Respond with one thing.
+// GET /blog/:id - - - - - Respond with one thing.
+router.get('/featured', (req, res) => {
+    const queryText = `
+    SELECT * FROM "blogs" ORDER BY "id" DESC LIMIT 1;`
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
-// POST /things - - - - - -  Create one thing.
+// POST /blog - - - - - -  Create one thing.
 
-// DELETE /things/:id - - -  Delete one thing.
+// DELETE /blog/:id - - -  Delete one thing.
 
-// PUT /things/:id - - - - - Update one thing.
+// PUT /blog/:id - - - - - Update one thing.
 
 
 module.exports = router;
