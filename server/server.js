1240d08735760cdd15aaecb4d0bd2b0836a75ba0
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-26 15:20:13 -0600

    updated pool for my db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const thingsRouter = require('./routes/things.router.js');
+const blogRouter = require('./routes/blog.router.js');
 
 const PORT = 5001;
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.static('server/public'));
-app.use('/things', thingsRouter);
+app.use('/blog', blogRouter);
 
 app.listen(PORT, () => {
   console.log(`server is up and running at http://localhost:${PORT}`)
