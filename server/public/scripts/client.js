94fa614b7ad45930ea11059b4abc6ebb19d89022
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-05 17:17:51 -0600

    created and outsourced a axiosmethods file

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -194,37 +194,6 @@ function handleUpdate(event) {
 
 }
 
-// ! Render
-/**
- * To render the post titles to the side bar for navigation
- */
-function getPostList() {
-    axios({
-        method: "GET",
-        url: "/blog"
-    }).then((response) => {
-        renderPostList(response);
-    }).catch((err) => {
-        console.log(err);
-    })
-}
-
-/**
- * GETs the newest post from the database, and sets it as the currentPost, and calls the renderFeaturedPost function. 
- */
-function getFeatured() {
-
-    axios({
-        method: "GET",
-        url: "blog/featured"
-    }).then((response) => {
-        setCurrentPost(response.data[0])
-        renderFeaturedPost(response)
-    }).catch((error) => {
-        console.log(error);
-    })
-}
-
 // ! State
 let writeMode = false
 let currentPost
