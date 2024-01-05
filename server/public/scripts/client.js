48a2c8eaad345d77e9abe7d6b34ea0af720f5477
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-05 17:15:01 -0600

    added a render file sourced in

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -209,30 +209,6 @@ function getPostList() {
     })
 }
 
-// ! A rendering function doc
-/**
- * Renders a response object from a GET request to the DOM
- * @param {object} response GET object
- */
-function renderPostList(response) {
-    const container = document.getElementById('post-list');
-    let postList = response.data;
-    container.innerHTML = '';
-    if (postList.length === 0) {
-        container.innerHTML = `Nothing here, yet! ✏️`;
-    }
-    for (post of postList) {
-        container.innerHTML += `
-            <div class="row"
-                <div class="col">
-                 <button class="btn" onclick="handleShowPost(${post.id})">${post.title}<span> ${post.inserted_at}</span></button>
-                </div>
-            </div>
-            <div class="line"></div>
-            `;
-    }
-}
-
 /**
  * GETs the newest post from the database, and sets it as the currentPost, and calls the renderFeaturedPost function. 
  */
@@ -247,47 +223,6 @@ function getFeatured() {
     }).catch((error) => {
         console.log(error);
     })
-}
-
-// ! A rendering function doc
-/**
- * This function handles the rendering of the current post to the page.
- * It will check to make sure that there is data to show, then will
- * render the currentPost if there is one.
- * Is called when a new post is added and the page loads, but not when a post is selected from the 
- * sidebar. 
- * @param {Object} response the response from the GET request. 
- */
-function renderFeaturedPost(response) {
-    if (response.data.length === 0) {
-        renderNoFeatured()
-    } else {
-        const container = document.getElementById('current-post')
-        const header = document.getElementById('showing')
-
-        header.innerText = 'Featured Post'
-        container.innerHTML = `
-            <h2>${currentPost.title}</h2>
-            <small>${currentPost.inserted_at}</small>
-            ${currentPost.body}
-    `
-        reactivateButton('edit-btn', handleEdit, currentPost.id)
-        reactivateButton('delete-btn', handleDelete, currentPost.id)
-    }
-}
-
-// ! Render functions doc
-/**
- * Function that renders a special message and disables delete and 
- * edit buttons if there are no blogs to be shown. 
- */
-function renderNoFeatured() {
-    const container = document.getElementById('current-post')
-    const header = document.getElementById('showing')
-    container.innerHTML = 'What will you write about? ☁️'
-    header.innerText = 'Featured Post'
-    disableButton('delete-btn')
-    disableButton('edit-btn')
 }
 
 // ! State
