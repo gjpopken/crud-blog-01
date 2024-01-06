94c7916c73b97d043812975c66ffaeb29e44c1de
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-06 11:25:01 -0600

    configured so that I can deploy to HerokuA

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,20 @@
-const pg = require('pg'); 
+const pg = require('pg');
+let pool
 
-const Pool = pg.Pool;
-
-
-const pool = new Pool({
-  database: 'blog-app-01',
-  host: 'localhost',
-  port: 5432,
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+} else {
+  pool = new pg.Pool({
+    database: 'blog-app-01',
+    host: 'localhost',
+    port: 5432,
+  });
+}
 
 pool.on('connect', () => {
   console.log('Postgresql connected!');
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const blogRouter = require('./routes/blog.router.js');
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
